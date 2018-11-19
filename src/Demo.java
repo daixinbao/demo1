9ddc82c270dbbb8a2840184f8f3991804d6a4b21
代新宝 <37148823+daixinbao@users.noreply.github.com>
2018-11-19 23:45:28 +0800

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,9 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.printf("aaaaabbaa");
+        System.out.printf("adsa");
+        System.out.printf("");
+        System.out.printf("aassdsad");
+        System.out.printf("aaaaabadsadbaa");
     }
 }
 
